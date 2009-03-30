c1986fdefdc7d32e4943036d1ead69f62392fa36
Samiuela LV Taufa <sam@nullcube.com>
2009-03-30 17:45:23 +1100

    Documentation Update   - update to current cs format   -

---
+++ doc-src/packet/index.py
@@ -0,0 +1,8 @@
+from countershape.doc import *
+
+pages = [
+    Page("creating.html","Creating"),
+    Page("packet.html","Packet"),
+    Page("protocol.html","Protocol"),
+    Page("apiref.html","API Reference"),
+]
